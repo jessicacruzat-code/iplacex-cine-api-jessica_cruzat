b2f6634ea06c56b93c50cda806510a9eb550325d
jessicacruzat-code <jessicacruzat@gmail.com>
2026-07-18 14:57:23 -0400

    Update db.js
    modificación pass

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://jessicacruzat_db_user:Q2m4Y7BdZOML9HCE@eva-u3-express.qvkjdf9.mongodb.net/?appName=eva-u3-express";
+const uri = "mongodb+srv://jessicacruzat_db_user:tON5znQIjXVLk7H4@eva-u3-express.qvkjdf9.mongodb.net/?appName=eva-u3-express";
 const client = new MongoClient(uri);
 const db = client.db("cine-db");
 
